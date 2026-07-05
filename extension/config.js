0621069fad3c025138e7302c5e220f77573dbebd
Rohit Sahu <rohitsahu.brandchanakya@gmail.com>
2026-07-05 17:57:46 +0530

    chore: point extension endpoints to production domain

--- extension/config.js
+++ extension/config.js
@@ -1,10 +1,10 @@
 // Change these to switch environments.
-const BASE_URL = "http://localhost:3457";
-// const BASE_URL = "https://aplusstudio.iprixmedia.com";
+const BASE_URL = "https://aplusstudio.iprixmedia.com";
+// const BASE_URL = "http://localhost:3457";
 
 // Frontend URL (for login redirect)
-const FRONTEND_URL = "http://localhost:3457";
-// const FRONTEND_URL = "https://aplusstudio.iprixmedia.com";
+const FRONTEND_URL = "https://aplusstudio.iprixmedia.com";
+// const FRONTEND_URL = "http://localhost:3457";
 
 const API_URL    = `${BASE_URL}/api/templates`;
 const FK_API_URL = `${BASE_URL}/api/flipkart/templates`;
